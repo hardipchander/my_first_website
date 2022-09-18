4a5550936d93a9f92cce3d5b95a697fc5ccb4e94
Hardip Chander <hardipchander@gmail.com>
2022-09-18 18:02:23 -0400

    Added the button to change the color of the page

---
+++ behaviour.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function ColorSwitch() {
+    if(document.body.style.color==='white') {
+        document.body.style.color='black';
+                
+    }
+    else {
+        document.body.style.color='white';
+    }    
+
+}
+
